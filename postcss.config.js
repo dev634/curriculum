9f5345cd79d760eaf8e54d20545e6d3ebad78ba3
Dev634 <pablolola@live.fr>
2022-01-22 19:20:33 +0100

    change workflow

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,25 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 const postcssImport = require("postcss-import");
 const cssnano = require("cssnano");
+const postcssPresetEnv = require("postcss-preset-env");
+const postcssNested = require("postcss-nested");
 
 module.exports = {
   map: false,
   plugins: [
     postcssImport(),
+    postcssNested(),
+    postcssPresetEnv({
+      stage: 2,
+    }),
     purgecss({
       content: ["index.html"],
       css: ["assets/custom.css"],
       keyframes: true,
       variables: true,
     }),
-    cssnano({
-      preset: "default",
-    }),
+    // cssnano({
+    //   preset: "default",
+    // }),
   ],
 };
